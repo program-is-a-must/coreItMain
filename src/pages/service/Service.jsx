273b6e6ca6f24a1bc540bb79c3ceb99a0ac63c0a
Balogun Mishael <balogunmishael7@gmail.com>
2025-05-23 16:10:58 +0100

    mobile section for service page

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -153,6 +153,148 @@ function Service() {
           </section>
         </div>
       </section>
+      <section class="fourth-content">
+        <div class="fourth-wrapper">
+          <div class="mission-block">
+            <div class="item">
+              <div class="text-1">
+                <div className="seperate-text">
+                  <h4>Our Mission</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore.
+                  </p>
+                </div>
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/case-5-2.jpg"
+                  alt="Mission"
+                />
+              </div>
+
+              <div className="text">
+                <div className="reduce" id="reduce-1"></div>
+
+                <p>Our Growth, Compare to Last Financial Year</p>
+                <span>2.5X</span>
+              </div>
+            </div>
+            <div class="item">
+              <div className="text">
+                <div className="reduce" id="reduce-2"></div>
+
+                <p>Our Growth, Compare to Last Financial Year</p>
+                <span>10k</span>
+              </div>
+              <div class="text-1">
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/price-tab-bg.jpg"
+                  alt="Vision"
+                />
+
+                <div className="seperate-text">
+                  <h4>Our Vision</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="text-1">
+                <div className="seperate-text">
+                  <h4>Our Core Values</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore.
+                  </p>
+                </div>
+
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2024/12/about-home-2.jpg"
+                  alt="Core Values"
+                />
+              </div>
+
+              <div className="text">
+                <div className="reduce" id="reduce-3"></div>
+                <p>High Retention Reflects our Commitment to Success.</p>
+                <span>67%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="fifth-content">
+        <div className="fifth-wrapper">
+          <div class="team-section">
+            <h2>Dedicated Team Professional Individuals</h2>
+            <div class="team-grid">
+              <div class="team-card">
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/team-1-min-768x640.jpg"
+                  alt="Team Member"
+                />
+                <h4>Mr. Drummond</h4>
+                <p>CEO & Founder</p>
+              </div>
+              <div class="team-card">
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/team-6-min-768x640.jpg"
+                  alt="Team Member"
+                />
+                <h4>Grace Tanwarah</h4>
+                <p>Design Strategist</p>
+              </div>
+              <div class="team-card">
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/team-3-min-768x640.jpg"
+                  alt="Team Member"
+                />
+                <h4>Felix Ekwem</h4>
+                <p>Lead Developer</p>
+              </div>
+            </div>
+            <button class="load-more">Load More</button>
+          </div>
+
+          <div class="testimonial-section">
+            <h3>Words from our satisfied clients</h3>
+            <div class="testimonials">
+              <div class="quote-box">
+                <div className="client-photos" id="client-photos">
+                  <img
+                    src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/testi-2.jpg"
+                    alt="pic"
+                  />
+                </div>
+                <p>
+                  "CoreIT gave me exactly what I needed. Their dedication and
+                  transparency is top-notch."
+                </p>
+                <strong>Jane Norbert</strong>
+                <br />
+                <small>Happy Client</small>
+              </div>
+              <div class="client-photos">
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/testi1-1.jpg"
+                  alt="Client"
+                />
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/testi1-3.jpg"
+                  alt="Client"
+                />
+                <img
+                  src="https://coreit.themepanthers.com/demo/wp-content/uploads/2025/01/testi1-2.jpg"
+                  alt="Client"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
